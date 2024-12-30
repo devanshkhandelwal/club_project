c0fd9ca72144bda88b91f0439157325e005595bd
devanshkhandelwal <devanshkhandelwal07@gmail.com>
2024-12-30 20:52:26 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((err) => console.error(err));
+
+app.get("/", (req, res) => res.send("API is running..."));
